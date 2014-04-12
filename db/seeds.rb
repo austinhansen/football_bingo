ed80fb1cc613e61f3f4bb8f85e1c6337b6eff948
Austin Hansen <austinthansen@gmail.com>
2014-04-12 09:32:08 -0600

    more items for seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@
 #
 # Examples:
 #
-Phrase.create([{ line: 'Pace' }, { line: 'Brave' }, { line: 'Cynical foul' }, { line: 'For my money/For me' }, { line: 'Any day of the week' }, { line: 'Stone penalty' }, { line: 'Penalty kick' }, { line: 'Red card' }, { line: 'Yellow card' }, { line: 'Nutmeg' }, { line: 'Free kick' }, { line: 'Volley' }, { line: 'Stone penalty' }])
+Phrase.create([{ line: 'Pace' }, { line: 'Brave' }, { line: 'Cynical foul' }, { line: 'For my money/For me' }, { line: 'Any day of the week' }, { line: 'Stone penalty' }, { line: 'Penalty kick' }, { line: 'Red card' }, { line: 'Yellow card' }, { line: 'Nutmeg' }, { line: 'Free kick' }, { line: 'Volley' }, { line: 'Bicycle kick' }, { line: 'Free header' }, { line: 'Header' }, { line: 'Long ball' }, { line: 'Referee' }, { line: 'Pitch' }, { line: 'Good cross' }, { line: 'Poor cross' }, { line: 'Hit the post/crossbar' }, { line: 'Injury' }, { line: 'Substitution' }, { line: 'Well worked' }, { line: 'Counterattack' }, { line: 'Goal' }, { line: 'Europe' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
