5ddb1f02a34b0d4af19549bdf6cd4f6f7a26b64f
Austin Hansen <austinthansen@gmail.com>
2014-04-26 18:20:32 -0600

    removed notice from card creation

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -28,7 +28,7 @@ class CardsController < ApplicationController
     @card.phrases = Phrase.order("RANDOM()").limit(25)
     respond_to do |format|
       if @card.save
-        format.html { redirect_to @card, notice: 'Card was successfully created.' }
+        format.html { redirect_to @card }
         format.json { render action: 'show', status: :created, location: @card }
       else
         format.html { render action: 'new' }
